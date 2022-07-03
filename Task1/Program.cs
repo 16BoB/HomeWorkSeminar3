bf192537b26c5d46d959a91a47bfac57de59a660
Tape <trid.skivi@gmail.com>
2022-07-03 16:29:38 +0300

    wrote the solution to the first task

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+void CheckPalindrome (int arg) {
+    int reversNum = 0;
+    int temp = arg;
+    while (temp != 0) {
+        reversNum = reversNum * 10 + temp % 10;
+        temp /= 10;
+    }
+    if (arg == reversNum) Console.WriteLine("The number is a polydrome");
+    else Console.WriteLine("The number is not a polydrome");
+}
+
+Console.WriteLine("Enter a five-digit number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+CheckPalindrome(num);
